6600da06ba3900afa7decccfb0a7d89ab8bb3c3e
Logan Miller <wrr2216@gmail.com>
2021-11-04 11:45:31 -0500

    Changed isStaked to string to accomadate different inputs other than True / False

--- CryptoLedger/Asset.cs
+++ CryptoLedger/Asset.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
-namespace CryptoLedger
+﻿namespace CryptoLedger
 {
     public class Asset
     {
@@ -12,15 +6,6 @@ namespace CryptoLedger
         public decimal Amount { get; set; }
         public decimal Invested { get; set; }
         public string Wallet { get; set; }
-        public bool isStaked { get; set; }
+        public string isStaked { get; set; }
     }
 }
-
-
-/*
- string Ticker
-int Amount
-int Invested
-string Wallet
-bool isStaked
- */
