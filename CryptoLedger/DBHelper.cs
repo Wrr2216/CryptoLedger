be3785ffe59a1a7ac3d5b8bdad359a84422b66a6
Logan Miller <logan@millercyber.tech>
2021-11-05 15:17:52 -0500

    Added sort by Descending order from SQL Data

--- CryptoLedger/DBHelper.cs
+++ CryptoLedger/DBHelper.cs
@@ -42,6 +42,45 @@ namespace CryptoLedger
 
         }
 
+        public void updateDBMarket()
+        {
+            ConsoleHelper ch = new ConsoleHelper();
+            Asset _tempAsset = new Asset();
+
+            using var _connection = new SQLiteConnection(_database);
+            try
+            {
+                _connection.Open();
+
+                using var cmd = new SQLiteCommand(_connection);
+                cmd.CommandText = "SELECT * FROM assets";
+                cmd.Prepare();
+                SQLiteDataReader _reader = cmd.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _tempAsset = new Asset()
+                    {
+                        Ticker = Convert.ToString(_reader.GetValue(0)),
+                        Amount = Convert.ToDecimal(_reader.GetValue(1)),
+                        Invested = Convert.ToDecimal(_reader.GetValue(2)),
+                        Wallet = Convert.ToString(_reader.GetValue(3)),
+                        isStaked = Convert.ToString(_reader.GetValue(4)),
+                        marketVal = Convert.ToDecimal(_reader.GetValue(5))
+                    };
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                ch.LogErr(ex.Message, 0);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
         public void addAsset(string dTicker, decimal dAmount, decimal dInvested, string dWallet, string disStaked, decimal dCv)
         {
             ConsoleHelper ch = new ConsoleHelper();
@@ -148,7 +187,7 @@ namespace CryptoLedger
             try
             {
                 _connection.Open();
-                string query = "SELECT * FROM assets";
+                string query = "SELECT * FROM assets ORDER BY invested DESC";
                 using var cmd = new SQLiteCommand(query, _connection);
                 using SQLiteDataReader _reader = cmd.ExecuteReader();
 
